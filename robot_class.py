7a49ea86219f6957c5fde08d41786da074969ed6
s6xybr8in <s6xybr8in@gmail.com>
2023-12-01 12:44:49 +0900

    complete algorithm but obstacle dont'

--- robot_class.py
+++ robot_class.py
@@ -1,25 +1,24 @@
 class robot:
-    def __init__(self,coordinate,direction,straight_speed,rotate_speed,stop,goal):
+    def __init__(self,coordinate,direction,straight_speed,rotate_speed,stop,goal,color):
         self.coordinate = coordinate   # initial location
         self.direction = direction     # initial direction 00 : front, 11 : back , 01 : left, 11 : right
-        self.idx = None
         self.path = None               # robot's path [] 
         self.straight = straight_speed
         self.rotate = rotate_speed
         self.stop = stop
         self.goal = goal
-    def put_idx(self,color):
         if color == 'R':
-            self.idx = 11
+            self.idx = 1
         elif color == 'G':
-            self.idx = 12
+            self.idx = 2
         elif color == 'B':
-            self.idx = 13
+            self.idx = 3
         else: 
-            self.idx = 14
+            self.idx = 4
 
-    def put_path(self,path):
-        self.path = path
+    def put_path(self,Path):
+        
+        self.path = str(self.idx)+":"+Path
     def put_speed(self,straight_speed,rotate_speed):
         self.straight = straight_speed
         self.rotate = rotate_speed
