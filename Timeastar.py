78ccf46a88179394354ac253d6d3d9fcc4d10d5f
s6xybr8in <s6xybr8in@gmail.com>
2023-12-02 14:53:31 +0900

    optimize2

--- Timeastar.py
+++ Timeastar.py
@@ -5,22 +5,22 @@ from math import *
 from robot_class import robot as Robot
 
 class Node:
-    def __init__(self,parent, coordinate, cost, heuristic,dir):
-        self.parent = parent
-        self.coordinate = coordinate
-        self.cost = cost
-        self.heuristic = heuristic
-        self.dir = dir
+    def __init__(self,parent :object, coordinate:tuple, cost, heuristic,dir):
+        self.PARENT = parent
+        self.COORDINATE = coordinate
+        self.COST = cost
+        self.HEURISTIC = heuristic
+        self.DIRCTION = dir  # 0b00 : front  0b11 : back 0b01 : left 0b10: right
         
     def __lt__(self, other):
-        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+        return (self.COST + self.HEURISTIC) < (other.COST + other.HEURISTIC)
     def __eq__(self, other) -> bool:
         if isinstance(other, Node):
-            if self.coordinate == other.coordinate and self.heuristic == other.heuristic and self.cost == other.cost:
+            if self.COORDINATE == other.COORDINATE and self.HEURISTIC == other.HEURISTIC and self.COST == other.COST:
                 return True
         return False
     def __hash__(self) -> int:
-        return self.cost+self.heuristic
+        return self.COST+self.HEURISTIC
 
 
 class TimeAstar:
@@ -32,6 +32,7 @@ class TimeAstar:
         self.TimeTable = [[[[0,0]  for _ in range(len(robots))] for _ in range(SIZE)] for _ in range(SIZE)]
         self.COST_RATIO = 5
         self.set_goal(goal)
+
     def set_goal(self,goal:tuple):
         for robot in self.robots:
             robot.goal = goal
@@ -66,58 +67,57 @@ class TimeAstar:
     def CCW(self,A, B):
     # A와 B의 외적 계산
         cross_product = np.cross(A, B)
-        # print(A,B,"= ",cross_product)
 
-        # 외적의 부호에 따라 시계방향 또는 반시계방향 여부 판단
         if cross_product > 0 : return 0
         elif cross_product < 0 : return 1
         return 2
 
-    def lightweight(self,vector_list:list):
-        i = 0
-        modified_list = []
-        command_list = []
-        dir = { 
-            (0,0) : 'S',
-            (0,1) : 'F', 
-            (1,0) : 'F',
-            (0,-1) : 'B',
-            (-1,0) : 'B',
-            }
+    def ToCommand(self,idx :int):
+        # i = 0
+        # modified_list = []
+        # command_list = []
+        # dir = { 
+        #     (0,0) : 'S',
+        #     (0,1) : 'F', 
+        #     (1,0) : 'F',
+        #     (0,-1) : 'B',
+        #     (-1,0) : 'B',
+        #     }
 
-        while i < len(vector_list) - 1:
-            j = i + 1
-            while j < len(vector_list) and vector_list[i] == vector_list[j]:
-                j += 1
+        # while i < len(vector_list) - 1:
+        #     j = i + 1
+        #     while j < len(vector_list) and vector_list[i] == vector_list[j]:
+        #         j += 1
 
-            ccw = self.CCW(modified_list[-1], vector_list[j - 1]) if modified_list else None
-            R = ("R90", "R-90", "")[ccw] if ccw is not None else ""
-            if R:
-                command_list.append(R)
+        #     ccw = self.CCW(modified_list[-1], vector_list[j - 1]) if modified_list else None
+        #     R = ("R90", "R-90", "")[ccw] if ccw is not None else ""
+        #     if R:
+        #         command_list.append(R)
 
-            if vector_list[i] != (0, 0):
-                modified_list.append(vector_list[i])
-            command_list.append(dir[vector_list[i]] + str(j - i))
-            i = j
+        #     if vector_list[i] != (0, 0):
+        #         modified_list.append(vector_list[i])
+        #     command_list.append(dir[vector_list[i]] + str(j - i))
+        #     i = j
 
-        else:
-            if i < len(vector_list):
-                command_list.append(dir[tuple(vector_list[i])] + "1")
+        # else:
+        #     if i < len(vector_list):
+        #         command_list.append(dir[tuple(vector_list[i])] + "1")
 
-        return '/'.join(command_list)
+        # return '/'.join(command_list)
+        return 'ab'
 
     def path_tracking(self,idx:int,T_Node : Node) -> None:
         List = []
         Table = self.TimeTable
-        while(T_Node.parent is not None):
-            y,x = T_Node.coordinate
-            Table[y][x][idx] = [T_Node.cost - T_Node.parent.cost,T_Node.cost]
-            print(T_Node.coordinate,T_Node.cost)
-            List.append((x-T_Node.parent.coordinate[1],y-T_Node.parent.coordinate[0]))
-            T_Node = T_Node.parent
+        while(T_Node.PARENT.COORDINATE is not None):
+            y,x = T_Node.COORDINATE
+            Table[y][x][idx] = [T_Node.COST - T_Node.PARENT.COST,T_Node.COST]
+            print(T_Node.COORDINATE,T_Node.COST)
+            List.append((x-T_Node.PARENT.COORDINATE[0],y-T_Node.PARENT.COORDINATE[1]))
+            T_Node = T_Node.PARENT
         List.reverse()
         print(List)
-        # self.robots[idx].put_path(self.lightweight(List))
+        # self.robots[idx].put_path(self.ToCommand(List))
         
     def Robot_sort(self):
         self.robots.sort(key=lambda x : self.distance(x.coordinate,x.goal))
@@ -125,28 +125,28 @@ class TimeAstar:
     def Search(self,idx:int) -> None: # Robot Path finding 
         # Heuristic = Distance  // F = G(현재까지 온 거리) + H(맨하튼 거리)
         
-        robot = self.robots[idx]
-        GOAL = robot.GOAL
-        SPEED = robot.straight*self.COST_RATIO
-        ROTATE = robot.rotate*self.COST_RATIO
-        STOP = robot.stop*self.COST_RATIO
+        ROBOT = self.robots[idx]
+        GOAL = ROBOT.GOAL
+        SPEED = ROBOT.STRAIGHT*self.COST_RATIO
+        ROTATE = ROBOT.ROTATE*self.COST_RATIO
+        STOP = ROBOT.STOP*self.COST_RATIO
 
-        Q = [ Node(None, robot.coordinate , 0, self.distance(robot.coordinate , GOAL), robot.direction)]
+        Q = [ Node(None, ROBOT.coordinate , 0, self.distance(ROBOT.coordinate , GOAL), ROBOT.direction)]
         visited = set()
         while Q:
             Top = Pop(Q)
-            visited.add(Top.coordinate)
+            visited.add(Top.COORDINATE)
             
-            for dir,ku in enumerate([[0,1],[0,-1],[1,0],[-1,0],[0,0]]):
-                y = ku[1] + Top.coordinate[1]
-                x = ku[0] + Top.coordinate[0]
+            for dir,MOV in enumerate([[0,1],[0,-1],[1,0],[-1,0],[0,0]]):
+                y = MOV[1] + Top.COORDINATE[1]
+                x = MOV[0] + Top.COORDINATE[0]
                 if dir == 4:
-                    Push(Q, Node(Top,Top.coordinate,Top.cost+STOP,Top.heuristic,Top.dir) )
+                    Push(Q, Node(Top,Top.COORDINATE,Top.COST+STOP,Top.HEURISTIC,Top.DIRCTION))
                 else:
                     if x < 0 or y < 0 or x > self.SIZE-1 or y > self.SIZE-1 or self.MAP[y][x]== -1 or (x,y) in visited: continue
-                    st = Top.cost+ROTATE+SPEED
+                    st = Top.COST+ROTATE+SPEED
                     if dir ^ Top.dir in (0,3):
-                        st = Top.cost+SPEED
+                        st = Top.COST+SPEED
                     if all(map(lambda r: (st < r[0]  or st > r[1]),self.TimeTable[y][x])):
                         if((x,y) == GOAL): #success path find!
                             self.path_tracking(idx,Node(Top,(x,y),st,self.distance((x,y),GOAL),dir))
--- robot_class.py
+++ robot_class.py
@@ -1,24 +1,24 @@
 class robot:
     def __init__(self,coordinate:tuple,direction:int,straight_speed:int,rotate_speed:int,stop:int,color:str):
         self.coordinate = coordinate   # initial location
-        self.direction = direction     # initial direction 00 : front, 10 : back , 01 : left, 11 : right
+        self.direction = direction     # initial direction [00 : front], [11 : back] , [01 : left], [10 : right]
         self.path = None               # robot's path [] 
-        self.straight = straight_speed
-        self.rotate = rotate_speed
-        self.stop = stop
-        self.goal = None
+        self.STRAIGHT = straight_speed
+        self.ROTATE = rotate_speed
+        self.STOP = stop
+        self.GOAL = None
         if color == 'R':
-            self.idx = 1
+            self.IDX = 1
         elif color == 'G':
-            self.idx = 2
+            self.IDX = 2
         elif color == 'B':
-            self.idx = 3
+            self.IDX = 3
         else: 
-            self.idx = 4
+            self.IDX = 4
 
     def put_path(self,Path)->None:
-        self.path = str(self.idx)+":"+Path
+        self.path = str(self.IDX)+":"+Path
 
     def put_speed(self,straight_speed,rotate_speed):
-        self.straight = straight_speed
-        self.rotate = rotate_speed
+        self.STRAIGHT = straight_speed
+        self.ROTATE = rotate_speed
